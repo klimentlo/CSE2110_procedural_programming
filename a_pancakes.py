c0d069ace77826ae6864edde57e357726d7c8613
k.lo1 <k.lo1@share.epsb.ca>
2022-09-14 15:20:29 -0600

    PANCAKE THINGY

---
+++ a_pancakes.py
@@ -0,0 +1,27 @@
+# a_pancakes.py
+
+'''
+title: Pancake Calculator
+author: Kliment Lo
+date-created: 2022-09-22
+'''
+
+# --- SUBROUTINES --- #
+
+### INPUTS
+def getFlour():
+    '''
+    Ask user for amount of flour in cups
+    :return: (float)
+    '''
+    FLOUR = float(input("How many cups of flour do you have? "))
+    return FLOUR
+
+### PROCESSING
+
+### OUTPUTS
+
+
+# --- MAIN PROGRAM CODE --- #
+ANSWER = getFlour()
+print (ANSWER)
