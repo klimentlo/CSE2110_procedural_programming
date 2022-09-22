d15902392865b8f4ef246dfe5919313a660d74bb
k.lo1 <k.lo1@share.epsb.ca>
2022-09-22 15:20:58 -0600

    New

--- d_summative_cannon_project.py
+++ d_summative_cannon_project.py
@@ -6,15 +6,48 @@ date-created: 2022-09-21
 '''
 # --- FUNCTIONS --- #
 
-
 ### INPUTS
+def menu():
+    '''
+    User selects which math operation to perform
+    :return:
+    '''
+    print('''
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Sin(x)
+    ''')
+    CHOICE = input("Choose an operation to perform: ")
+    CHOICE = checkInt(CHOICE)
+    if CHOICE > 0 and CHOICE < 6:
+        return CHOICE
+    else:
+        print("Please select a valid number! ")
+        return menu()
 
 
 ### PROCESSING
 
 
 ### OUTPUTS
-
+def intro():
+    '''
+    tells the user what the program does and provides an introduction
+    :return: (none)
+    '''
+    print ('''
+    ___
+   |   \\
+   |    \\
+   |     \\
+   return 
+   
+    
+    ''')
 
 
 # --- MAIN PROGRAM --- #
+
+intro()
