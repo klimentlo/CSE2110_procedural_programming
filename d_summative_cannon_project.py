5d2886a0888bf6bf100852df9065ce1c2ae26eb6
Kliment Lo <Kliment Lo>
2022-09-28 21:39:37 -0600

    Ooof

--- d_summative_cannon_project.py
+++ d_summative_cannon_project.py
@@ -14,36 +14,25 @@ def menu():
     User selects which Scenario they want to calculate
     :return:
     '''
-    print('''
-
-
-  Scenario 1:                 |        Scenario 2:                     |         Scenario 3:
-    ____                      |            ___                         |            ____
-    |   \\                     |           /   \\                        |           /    \\
-    |    \\                    |          /     \\                       |          |      \\
-    |     \\                   |         /       \\                      |          |       \\
-    Horizontal to the water   |         Parabolic to a level boat      |          Parabolic to a smaller boat far away
+    print('''-------------------------------------------------------------------------------------------------------------------------------------------
+|                                  |                                  |                                  |                                |
+|      Scenario 1:                 |         Scenario 2:              |         Scenario 3:              |         Scenario 4:            |
+|        ____                      |             ___                  |            ____                  |            ____                |
+|        |   \\                     |            /   \\                 |           /    \\                 |           /    \\               |   
+|        |    \\                    |           /     \\                |          |      \\                |          /      \\              |   
+|        |     \\                   |          /       \\               |                  \\               |         /                      |
+|       Horizontal to the water    |     Parabolic to a level boat    |    Parabolic to a smaller boat   |   Parabolic to a larger boat   |
+|                                  |                                  |    boat far away                 |   far away                     |
+-------------------------------------------------------------------------------------------------------------------------------------------
     ''')
 
     CHOICE = input("Select a scenario you'd like to calculate: ")
-    CHOICE = checkInt(CHOICE)  # sends the value to the checkInt function, to see if the input is an integer (1, 2, 3, etc.)
-    CHOICE = validNum(CHOICE)
+    CHOICE = checkChoice(CHOICE)  # sends the value to the checkChoice function, to see if the input is an integer (1, 2, 3, etc.)
+    CHOICE = validChoice(CHOICE)
     return CHOICE
-
-def validNum(NUMBER):
+def checkChoice(NUMBER):
     '''
-    Checks if the number inputted by the user is a number between 1 and 4
-    :param NUMBER: (int)
-    :return:
-    '''
-    if NUMBER < 1 and NUMBER > 4:
-        print(f"{NUMBER} isn't an option. Please select a valid number! ")
-    else:
-
-        return NUMBER
-def checkInt(NUMBER):
-    '''
-    Verifies the number is an integer
+    Verifies the CHOICE is an integer
     :param NUMBER: (str)
     :return: (int)
     '''
@@ -52,9 +41,21 @@ def checkInt(NUMBER):
     else:
         print(f"{NUMBER} is not a valid option")
         NEW_NUM = input("Please select a scenario from 1 to 4: ")
-        # It now takes the new imput (or new number) and returns it to checkInt. It then runs the new number into checkint. If the new imput isn't        an integer, itll run this program again
-        return checkInt(NEW_NUM)
-
+        # It now takes the new imput (or new number) and returns it to checkChoice. It then runs the new number into checkChoice. If the new input isn't an integer, itll run this program again
+        return checkChoice(NEW_NUM)
+def validChoice(CHOICE):
+    '''
+    Verifies the number is in the number range
+    :param CHOICE: (str)
+    :return: (int)
+    '''
+    if CHOICE > 0 and CHOICE < 5:
+        return int(CHOICE)
+    else:
+        print("That is not one of the options! ")
+        NEW_CHOICE = input("Please select a scenario from 1 to 4: ")
+        CHOICE = int(NEW_CHOICE) #The newly inputted answer is turned into an integer, and CHOICE
+        return validChoice(CHOICE)
 def isNum(NUMBER):
     '''
     Checks if the input is a number or not
@@ -69,21 +70,6 @@ def isNum(NUMBER):
 You did not enter a number! """ )
         NEW_NUM = input("Please enter a number: ")
         return isNum(NEW_NUM)
-
-def validNum(CHOICE):
-    '''
-    Verifies the number is in the number range
-    :param CHOICE: (str)
-    :return: (int)
-    '''
-    if CHOICE > 0 and CHOICE < 5:
-        return int(CHOICE)
-    else:
-        print("That is not one of the options! ")
-        NEW_CHOICE = input("Please select a scenario from 1 to 4: ")
-        CHOICE = int(NEW_CHOICE) #The newly inputted answer is turned into an integer, and CHOICE
-        return validNum(CHOICE)
-
 
 def checkNeg(NUMBER):
     '''
@@ -129,15 +115,21 @@ def askContinue():
         return askContinue()
 
 def direction(DISTANCE):
-    if DISTANCE >= 0:
-        DIRECTION = str("N")
-        return DIRECTION
-    else:
-        DIRECTION = str("S")
-        return DIRECTION
+    try:
+        if DISTANCE >= 0:
+            DIRECTION = str("N")
+            return DIRECTION
+        else:
+            DIRECTION = str("S")
+            return DIRECTION
+    except TypeError:
+        print("Your cannonball wasn't shot fast enough to reach the ship! Please retry with Numbers")
+        if not askContinue():
+            exit()
+        return ""
 ### PROCESSING
 ## SCENARIO 1
-def timeCal(HEIGHT): #SCENARIO 1
+def timeCal(HEIGHT, GRAVITY): #SCENARIO 1
     '''
     Calculating the amount of time it is in the air
     :param HEIGHT: (str)
@@ -146,7 +138,7 @@ def timeCal(HEIGHT): #SCENARIO 1
     HEIGHT = isNum(HEIGHT)
     HEIGHT = checkNeg(HEIGHT)
     HEIGHT = float(HEIGHT)
-    SOLVE =  2 * HEIGHT / 9.81 #Uses the height the user inputted and puts it into formula
+    SOLVE =  2 * HEIGHT / GRAVITY #Uses the height the user inputted and puts it into formula
     TIME = SOLVE ** 0.5 #Squares the value, so we can get the ACTUAL time
     return TIME #inputs TIME as the value of timeCal(HEIGHT)
 
@@ -184,8 +176,8 @@ def velocityY(VELOCITY, ANGLE):
     return VELOCITYY
 
 
-def timeCal2(VELOCITYY):
-    TIME = VELOCITYY * 2 /9.81
+def timeCal2(VELOCITYY, GRAVITY):
+    TIME = VELOCITYY * 2 /GRAVITY
     if TIME < 0:
         TIME *= -1
     return float(TIME)
@@ -195,28 +187,52 @@ def distanceCal2(VELOCITYX, TIME):
     return DISTANCE
 
 ##SCENARIO 3
-def timePeak(VELOCITYY):
-    TIMEPEAK = VELOCITYY/9.81
+def timePeak(VELOCITYY, GRAVITY):
+    TIMEPEAK = VELOCITYY/GRAVITY
     return TIMEPEAK
 
-def maxHeight(VELOCITYY):
-    MAXHEIGHT = (VELOCITYY ** 2) / (2 * 9.81)
+def maxHeight(VELOCITYY, GRAVITY):
+    MAXHEIGHT = (VELOCITYY ** 2) / (2 * GRAVITY)
     return MAXHEIGHT
 
 def totHeight (MAXHEIGHT, SHIPHEIGHT):
-    #MAXHEIGHT = isNum(HEIGHTPEAK)
-    #SHIPHEIGHT = isNum(SHIPHEIGHT)
+    MAXHEIGHT = isNum(MAXHEIGHT)
+    SHIPHEIGHT = isNum(SHIPHEIGHT)
     HEIGHTTOTAL = MAXHEIGHT + SHIPHEIGHT
     return HEIGHTTOTAL
 
-def fallTime(TOTHEIGHT):
-    TIMEFALL = ((TOTHEIGHT * 2) / 9.81) ** 0.5
+def fallTime(TOTHEIGHT, GRAVITY):
+    TIMEFALL = ((TOTHEIGHT * 2) / GRAVITY) ** 0.5
     return TIMEFALL
 
 def distanceCal(VELOCITYX, TOTALTIME):
     DISTANCE = VELOCITYX * TOTALTIME
     return DISTANCE
-### OUTPUTS
+
+def totHeight4 (MAXHEIGHT, SHIPHEIGHT):
+    MAXHEIGHT = isNum(MAXHEIGHT)
+    SHIPHEIGHT = isNum(SHIPHEIGHT)
+    HEIGHTTOTAL = MAXHEIGHT - SHIPHEIGHT
+    return HEIGHTTOTAL
+
+def checkPossible(SHIPHEIGHT, MAXHEIGHT):
+    SHIPHEIGHT = isNum(SHIPHEIGHT)
+    SHIPHEIGHT = checkNeg(SHIPHEIGHT)
+
+    if SHIPHEIGHT > MAXHEIGHT:
+        NEW_SHIPHEIGHT = input(f"The shipheight is too high for the cannonball to reach! Please input a new height that is lower than {MAXHEIGHT}:")
+        return checkPossible(NEW_SHIPHEIGHT, MAXHEIGHT)
+    else:
+        return SHIPHEIGHT
+
+def totalTimeCal(TIMEPEAK, FALLTIME):
+    if TIMEPEAK < 0:
+        TIMEPEAK *= -1
+    if FALLTIME < 0:
+        FALLTIME *= -1
+    TOTALTIME = FALLTIME * TIMEPEAK
+    return TOTALTIME
+
 def intro():
     '''
     tells the user what the program does and provides an introduction
@@ -241,7 +257,10 @@ def main():
             HEIGHT = input("""
 How high above the water is the cannon located (in meters)? 
 """ )
-            HEIGHT = timeCal(HEIGHT) #calculates the time it is in the air using the height
+            GRAVITY = input("What is the magnitude of gravity? ")
+            GRAVITY = isNum(GRAVITY)
+            GRAVITY = checkNeg(GRAVITY)
+            HEIGHT = timeCal(HEIGHT, GRAVITY) #calculates the time it is in the air using the height
             VELOCITY = input("""How fast is the cannonball flying at when it leaves the cannon (in m/s)?" 
 """)
             VELOCITY = isNum(VELOCITY)
@@ -254,11 +273,18 @@ How high above the water is the cannon located (in meters)?
         if SCENARIO == 2: #if the user requested for scenario 2, the angled cannon towards parallel ship
             VELOCITY2 = input("What is the velocity of the cannonball as it leaves the cannon (m/s)?" )
             VELOCITY2= isNum(VELOCITY2)
+            #Requests for and calculates for angle
             ANGLE = input("What is the angle of the cannon to the ground? ")
             ANGLE = checkAngle(ANGLE)
+            #Converts the Velocity and anglegiven into velocityX and velocityY
             VELOCITYX = velocityX(VELOCITY2, ANGLE)
             VELOCITYY = velocityY(VELOCITY2, ANGLE)
-            TIME = timeCal2(VELOCITYY)
+            #Requests for gravity
+            GRAVITY = input("What is the magnitude of gravity? ")
+            GRAVITY = isNum(GRAVITY)
+            GRAVITY = checkNeg(GRAVITY)
+            #Calculates the time
+            TIME = timeCal2(VELOCITYY, GRAVITY)
             DISTANCE2 = distanceCal2(VELOCITYX, TIME)
             DIRECTION = direction(DISTANCE2)
             DISTANCE2 = round(DISTANCE2, 2)  # rounds the distance to two decimal places
@@ -273,25 +299,56 @@ The total distance the cannonball traveled was {DISTANCE2} meters {[DIRECTION]}.
             VELOCITY3 = isNum(VELOCITY3)
             ANGLE3 = input("What is the angle of the cannon to the ground? ")
             ANGLE3 = checkAngle(ANGLE3)
-            SHIPHEIGHT = input("How much lower is the enemy ship compared to yours? ")
+            SHIPHEIGHT = input("How much higher is the your ship compared to the enemies? ")
+            SHIPHEIGHT = isNum(SHIPHEIGHT)
             SHIPHEIGHT = checkNeg(SHIPHEIGHT)
+            GRAVITY = input ("What is the magnitude of gravity? ")
+            GRAVITY = isNum(GRAVITY)
+            GRAVITY = checkNeg(GRAVITY)
             VELOCITYX = velocityX(VELOCITY3, ANGLE3)
             VELOCITYY = velocityY(VELOCITY3, ANGLE3)
-            TIMEPEAK = timePeak(VELOCITYY)
-            MAXHEIGHT = maxHeight(VELOCITYY)
+            TIMEPEAK = timePeak(VELOCITYY, GRAVITY)
+            MAXHEIGHT = maxHeight(VELOCITYY, GRAVITY)
             TOTHEIGHT = totHeight(MAXHEIGHT, SHIPHEIGHT)
-            FALLTIME = fallTime(TOTHEIGHT)
+            FALLTIME = fallTime(TOTHEIGHT, GRAVITY)
             TOTALTIME = TIMEPEAK + FALLTIME
             DISTANCE3 = distanceCal(VELOCITYX, TOTALTIME)
             DISTANCE3 = round(DISTANCE3, 2)  # rounds the distance to two decimal places
             VELOCITYX = round(VELOCITYX, 2)  # rounds the velocityx to two decimal places
             VELOCITYY = round(VELOCITYY, 2)  # rounds the velocityy to two decimal places
-            TIME = round(TIME, 2)  # rounds the time to two decimal places
-            DIRECTION = direction(DISTANCE3)
+            TOTALTIME = round(TOTALTIME, 2)  # rounds the time to two decimal places
+            DIRECTION = direction(DISTANCE3, SHIPHEIGHT, MAXHEIGHT)
             print(f"""
 The cannonball is moving at {VELOCITYX}m/s horizontally and {VELOCITYY}m/s vertically, being in the air for a total of {TOTALTIME} seconds. 
 The total distance the cannonball traveled was {DISTANCE3} meters {[DIRECTION]}. """)
-
+        if SCENARIO == 4:
+            VELOCITY4 = input("What is the velocity of the cannonball as it leaves the cannon (m/s)?")
+            VELOCITY4 = isNum(VELOCITY4)
+            ANGLE4 = input("What is the angle of the cannon to the ground? ")
+            ANGLE4 = checkAngle(ANGLE4)
+            VELOCITYY = velocityY(VELOCITY4, ANGLE4)
+            SHIPHEIGHT = input("How much lower is the your ship compared to the enemies? ")
+            GRAVITY = input("What is the magnitude of gravity? ")
+            GRAVITY = isNum(GRAVITY)
+            GRAVITY = checkNeg(GRAVITY)
+            MAXHEIGHT = maxHeight(VELOCITYY, GRAVITY)
+            SHIPHEIGHT = checkPossible(SHIPHEIGHT, MAXHEIGHT)
+            VELOCITYX = velocityX(VELOCITY4, ANGLE4)
+            TIMEPEAK = timePeak(VELOCITYY, GRAVITY)
+            TOTHEIGHT = totHeight4(MAXHEIGHT, SHIPHEIGHT)
+            FALLTIME = fallTime(TOTHEIGHT, GRAVITY)
+            TOTALTIME = totalTimeCal(TIMEPEAK, FALLTIME)
+            DISTANCE4 = distanceCal(VELOCITYX, TOTALTIME)
+            MAXHEIGHT = round(MAXHEIGHT, 2)
+            DIRECTION = direction(DISTANCE4)
+            DISTANCE4 = round(DISTANCE4, 2)  # rounds the distance to two decimal places
+            VELOCITYX = round(VELOCITYX, 2)  # rounds the velocityx to two decimal places
+            VELOCITYY = round(VELOCITYY, 2)  # rounds the velocityy to two decimal places
+            TOTALTIME = round(TOTALTIME, 2)  # rounds the time to two decimal places
+            print(f"""
+            
+The cannonball is moving at {VELOCITYX}m/s horizontally and {VELOCITYY}m/s vertically, being in the air for a total of {TOTALTIME} seconds. 
+The total distance the cannonball traveled was {DISTANCE4} meters {[DIRECTION]}. """)
         if not askContinue():
             exit()
 
